ff6f7818397612262a2d5b016d3d0be34dee7b3e
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-07-05 22:33:47 +0200

    Update main_home.py

--- main_home.py
+++ main_home.py
@@ -1,60 +1,35 @@
 import discord
 from discord.ext import commands
-import random
 
-
-
-bot = commands.Bot(command_prefix='-', intents=discord.Intents.all(), case_insensitive=True)
+bot = commands.Bot(command_prefix='-', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print('Bot is ready')
-    try:
-        synced = await bot.tree.sync()
-        print(f"synced {len(synced)} commands(s)")
-    except Exception as e:
-        print(e)
+    synced = await bot.tree.sync()
+    print(synced)
+    print("Slash commands: " + str(len(synced)) + " commands")
 
-
-@bot.command()
-async def start_game(ctx, kap1 = discord.member, kap2 = discord.member, moderator1 = discord.member, moderator2 = discord.member):
+@bot.tree.command(name="looking_user", description="Looking for discord user")
+async def looking_user(ctx, interaction: discord.Interaction, capitan1: discord.Member = None, capitan2: discord.Member = None):
     user = ctx.author
-    if user.voice and user.voice.channel:
+    if user.voice and user.voice.channel and capitan1 and capitan2:
+        await interaction.response.send_message(f'User1: {capitan1.name} User2: {capitan2.name}')
         voice_channel = user.voice.channel
         members = voice_channel.members
         member_names = [member.name for member in members]
         len_players = len(members)
-        
-        # Для примера, выводим имена участников и их количество в текстовый канал
-        await ctx.send(f'Игроки в голосовом канале: {", ".join(member_names)}')
-        await ctx.send(f'Количество игроков: {len_players}')
 
-        if(member_names, len_players):
-            embed = discord.Embed(title="Добро пожаловать на фейсит", description="Капитаны, пикайте игроков", color=discord.Color.purple())
-            embed.set_thumbnail(url={})
-            embed.set_author(name="R1CONE faceit")
-            embed.add_field(name=f'Капитан 1 - {} - {} ', value=f'Игроки команды 1: {}')
-            embed.add_field(name=f'Капитан 2 - {} - {}', value=f'Игроки команды 2: {}')
-        
+        if len_players and member_names:
+            embed = discord.Embed(title="Welcome to discord fight 5v5", description="Kapitans, peak players", color=discord.Color.purple())
+            embed.set_thumbnail(url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRh6UJMDoGniGMJrj_UHk9fGlbSH0o8XR71w&s')
+            embed.set_author(name="5 V 5")
+            embed.add_field(name=f'Capitan 1 - {capitan1}', value=f'Players team 1: ')
+            embed.add_field(name=f'Capitan 2 - {capitan2}', value=f'Players team 2: ')
+            embed.add_field(name=f'unpicked plauers: {member_names}')
+
+
     else:
-        await ctx.send("Вы не находитесь в голосовом канале")
-        
-
-#part 2      
-@bot.command()
-async def help_command(ctx):
-    embed = discord.Embed(title="Help", description="this help for disassembly in using the bot", color=0x00ff00)
-    embed.set_author(name="5 v 5 discord")
-    embed.add_field(name="more text 2", value="Shows this help message", inline=False)
-    
-    embed.set_thumbnail(url='https://steamuserimages-a.akamaihd.net/ugc/1832417646113487031/C01A749B539CFF42C546C60BBF43E2C75ACCBD27/')
-    
-    # Add more fields here for other commands
-    embed.add_field(name="text field 1", value="Description for text field 1", inline=False)
-    embed.add_field(name="text field 2", value="Description for text field 2", inline=False)
-    embed.set_footer(text="Bot developed by YourName")
-
-    await ctx.send(embed=embed)
-        
+        await interaction.response.send_message('No member specified.')
 
 bot.run('')
