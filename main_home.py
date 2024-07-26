43478e8bb0f828c05eb8531ebf613ee818590fc2
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-07-26 20:03:28 +0200

    Update main_home.py

--- main_home.py
+++ main_home.py
@@ -1,8 +1,12 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import random
-    
+
+
 bot = commands.Bot(command_prefix='-', intents=discord.Intents.all())
+
+
 
 @bot.event
 async def on_ready():
@@ -12,38 +16,108 @@ async def on_ready():
     print("Slash commands: " + str(len(synced)) + " commands")
 
 @bot.tree.command(name="looking_user", description="Looking for discord user")
+@app_commands.describe(voice_channel_1="First voice channel", voice_channel_2="Second voice channel")
 async def looking_user(interaction: discord.Interaction, voice_channel_1: discord.VoiceChannel = None, voice_channel_2: discord.VoiceChannel = None):
     user = interaction.user
     if user.voice and user.voice.channel and voice_channel_1 and voice_channel_2:
         voice_channel = user.voice.channel
-        members = voice_channel.members #list with full information about everyone
+        members = voice_channel.members  # list with full information about everyone
         print(members)
-        member_names = [member.name for member in members] #list only with nicknames
-        print(member_names)
+        member_names = [member.name for member in members]
+
         await interaction.response.send_message(f'Members in voice channel: {", ".join(member_names)}')
-        
-        kapitan_names = random.sample(member_names, 2)
-        remaining_names = [name for name in member_names if name not in kapitan_names]
 
-
-    else:
-        await interaction.response.send_message('No member specified.')
-
-def Embed(kapitan_names, remaining_names, voice_channel_1, voice_channel_2):
-    member_team1 = []
-    member_team2 = []
-    global kap
-
-    if kap == 1:
-        embed = discord.Embed(title="Welcome to discord fight 5v5", description="Kapitans, peak players", color=discord.Color.purple())
+        embed = discord.Embed(title="Welcome to discord fight 5v5", description="Get ready to start game!", color=discord.Color.purple())
         embed.set_thumbnail(url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRh6UJMDoGniGMJrj_UHk9fGlbSH0o8XR71w&s')
         embed.set_author(name="5 V 5")
-        embed.add_field(name=f'Capitan 1 - {kapitan_names[0]}', value=f'Players team 1: {member_team1}')
-        embed.add_field(name=f'Capitan 2 - {kapitan_names[1]}', value=f'Players team 2: {member_team2}')
-    
-    if remaining_names:
-        embed.add_field(name='Unpicked players:', value='\n'.join([f'{i + 1} - {remaining_names[i]} ' for i in range(len(remaining_names))]), inline=False)
+        embed.add_field(name='Accept your game!', value=f'{", ".join(member_names)}')
+        
+        message = await interaction.followup.send(embed=embed)
+        await message.add_reaction('\u2705')
+
+        def check(reaction, user):
+            return str(reaction.emoji) == '\u2705' and user in members and reaction.message.id == message.id
+
+        reacted_members = set()
+        while len(reacted_members) < len(members):
+            reaction, user = await bot.wait_for('reaction_add', check=check)
+            reacted_members.add(user)
+
+        await start_message(interaction, members, member_names)
+        
+
+    elif not (voice_channel_1 and voice_channel_2):
+        await interaction.response.send_message('Both voice channels must be specified.')
+
     else:
-        embed.add_field(name=f'Game is ready', value=f'Click to accept game')
+        await interaction.response.send_message('You are not in a voice channel.')
+
+emoji = ['\u0031\uFE0F\u20E3', '\u0032\uFE0F\u20E3', '\u0033\uFE0F\u20E3', '\u0034\uFE0F\u20E3', '\u0035\uFE0F\u20E3', '\u0036\uFE0F\u20E3', '\u0037\uFE0F\u20E3', '\u0038\uFE0F\u20E3']
+list_com_1 = []
+list_com_2 = []
+kap = 1
+
+async def start_message(interaction, members, member_names):
+    kapitan_players = random.sample(member_names, 2)
+    members_slim = members
+
+    for player in kapitan_players:
+        members_slim.remove(player)
+        members_to_peack = members_slim
+
+
+    
+    kapitan1 = kapitan_players[0]
+    kapitan2 = kapitan_players[1]
+    print(f"Kapian1 name is {kapitan1}")
+    print(f"Kapian1 name is {kapitan2}")
+    for member in members:
+        if member.name == kapitan1:
+            kapitan1_id = member.id
+        elif member.name == kapitan2:
+            kapitan2_id = member.id
+    print(f"kap1 is {kapitan1_id}")
+    print(f"kap2 is {kapitan2_id}")
+
+    embed = discord.Embed(title="Welcome to R1CONE Faceit", description="Kapitan 1, peak your players!", color=discord.Color.purple())
+    embed.set_thumbnail(url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRh6UJMDoGniGMJrj_UHk9fGlbSH0o8XR71w&s')
+    embed.set_author(name="5 V 5")
+    embed.add_field(name='Kapitan 1 - ', value=f'{kapitan1}')
+    embed.add_field(name='Member of first team', value=f'{list_com_1}')
+    embed.add_field(name='Kapitan 2 - ', value=f'{kapitan2}')
+    embed.add_field(name='Member of second team', value=f'{list_com_2}')
+    embed.add_field(name='Remain players: ', value=f'{members_to_peack}')
+
+        
+    message = await interaction.followup.send(embed=embed)
+    len_play = len(members_to_peack)
+    reactions_to_add = emoji[:len_play]
+    for reaction in reactions_to_add:
+        await message.add_reaction(reaction)
+
+    if user.id == kapitan1.id:
+        if kap == 1:
+            if str(reaction) in emoji:
+                start_game(interaction, members, kapian1, kapian2, members_to_peack)
+                
+    
+    
+    
+
+async def start_game(interaction, members, kapian1, kapian2, members_to_peack):
+    if user.id == kapitan1.id:
+        if kap == 1:
+            if str(reaction) in emoji:
+                emoji_index = emoji.index(str(reaction))
+                selected_player = members_to_peack[emoji_index]
+                embed = discord.Embed(title="Welcome to R1CONE Faceit", description="Kapitan 1, peak your players!", color=discord.Color.purple())
+                embed.set_thumbnail(url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRh6UJMDoGniGMJrj_UHk9fGlbSH0o8XR71w&s')
+                embed.set_author(name="5 V 5")
+                embed.add_field(name='Kapitan 1 - ', value=f'{kapitan1}')
+                embed.add_field(name='Member of first team', value=f'{list_com_1}')
+                embed.add_field(name='Kapitan 2 - ', value=f'{kapitan2}')
+                embed.add_field(name='Member of second team', value=f'{list_com_2}')
+                embed.add_field(name='Remain players: ', value=f'{members_to_peack}')
+            
 
 bot.run('')
