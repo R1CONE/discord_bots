53754addae0ed104300c2c047bf6f02adcb3cdf8
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-12-29 23:31:53 +0100

    Update main_home.py

--- main_home.py
+++ main_home.py
@@ -10,7 +10,7 @@ bot = commands.Bot(command_prefix='-', intents=discord.Intents.all())
 bot.tracked_reactions = {}
 bot.member_names_dict = {}
 bot.voice_channels_dict = {}
-emoji = ['\u0031\uFE0F\u20E3', '\u0032\uFE0F\u20E3', '\u0033\uFE0F\u20E3', '\u0034\uFE0F\u20E3', '\u0035\uFE0F\u20E3', '\u0036\uFE0F\u20E3', '\u0037\uFE0F\u20E3', '\u0038\uFE0F\u20E3']
+emoji = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣']
 
 @bot.event
 async def on_ready():
@@ -25,26 +25,23 @@ async def looking_user(interaction: discord.Interaction, voice_channel_1: discor
     user = interaction.user
     if user.voice and user.voice.channel and voice_channel_1 and voice_channel_2:
         voice_channel = user.voice.channel
-        members = voice_channel.members  # list with full information about everyone
-        print(members)
+        members = voice_channel.members
         member_names = [member.name for member in members]
 
-        if len(member_names) <= 10: ## и больше 4
+        if len(member_names) <= 10:
             await interaction.response.send_message(f'Members in voice channel: {", ".join(member_names)}')
 
             embed = discord.Embed(title="Welcome to discord fight 5v5", description="Get ready to start game!", color=discord.Color.purple())
             embed.set_thumbnail(url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRh6UJMDoGniGMJrj_UHk9fGlbSH0o8XR71w&s')
             embed.set_author(name="5 V 5")
             embed.add_field(name='Accept your game!', value=f'{", ".join(f"@{name}" for name in member_names)}')
-        
+            
             message = await interaction.followup.send(embed=embed)
             await message.add_reaction('\u2705')
             
-            # Сохраняем данные каналов в словарь
             bot.tracked_reactions[message.id] = message
             bot.member_names_dict[message.id] = member_names
             bot.voice_channels_dict[message.id] = (voice_channel_1, voice_channel_2)
-
         else:
             await interaction.response.send_message('Anti-abuse system')
 
@@ -53,39 +50,33 @@ async def looking_user(interaction: discord.Interaction, voice_channel_1: discor
 
 @bot.event
 async def on_reaction_add(reaction, user):
+    if user.bot:
+        return
+
     message = reaction.message
     if message.id in bot.tracked_reactions:
-        if reaction.emoji == '\u2705':  # Проверяем, что это нужная реакция
-            print(f'User {user.name} reacted with {reaction.emoji}')  # nickname only
+        if reaction.emoji == '\u2705':
+            print(f'User {user.name} reacted with {reaction.emoji}')
             await message.channel.send(f'{user.mention} accepted the game!')
-            # Проверяем, есть ли пользователь уже в списке
             if user.name not in accepted_players:
                 accepted_players.append(user.name)
             
-            print(f"accepted players {len(accepted_players)}")
+            print(f"Accepted players {len(accepted_players)}")
             
             member_names = bot.member_names_dict.get(message.id, [])
             voice_channel_1, voice_channel_2 = bot.voice_channels_dict.get(message.id, (None, None))
 
-            print(sorted(member_names))
-            print(sorted(accepted_players))
-
-            # Если все игроки приняли, вызываем peaking_players
             if sorted(accepted_players) == sorted(member_names):
-                await peaking_players(message, user, accepted_players, voice_channel_1, voice_channel_2)
+                await peaking_players(message, accepted_players, voice_channel_1, voice_channel_2)
 
-async def peaking_players(message, user, accepted_players, voice_channel_1, voice_channel_2, interaction: discord.Interaction = None):
-    # Функционал команды, когда все игроки приняли участие
-    await message.channel.send("Starting the game!")
-
-    global kap
+async def peaking_players(message, accepted_players, voice_channel_1, voice_channel_2):
     kapitan_players = random.sample(accepted_players, 2)
-    kapitan1_nickname = kapitan_players[0]
-    kapitan2_nickname = kapitan_players[1]
+    kapitan1_nickname, kapitan2_nickname = kapitan_players
 
     last_players = [player for player in accepted_players if player not in kapitan_players]
-    list_com1 = []
-    list_com2 = []
+    list_com1, list_com2 = [], []
+
+    unpeaked_players = {index: player for index, player in enumerate(last_players)}
 
     embed = discord.Embed(
         title="Welcome to discord fight",
@@ -96,7 +87,7 @@ async def peaking_players(message, user, accepted_players, voice_channel_1, voic
     embed.set_author(name="discord battle")
     embed.add_field(name=f'Capitan 1 - @{kapitan1_nickname}', value=f'Players team 1: {list_com1}')
     embed.add_field(name=f'Capitan 2 - @{kapitan2_nickname}', value=f'Players team 2: {list_com2}')
-    embed.add_field(name='Unpeaked players:', value='\n'.join(last_players), inline=False)
+    embed.add_field(name='Unpeaked players:', value='\n'.join(unpeaked_players.values()), inline=False)
 
     message = await message.channel.send(embed=embed)
 
@@ -105,7 +96,7 @@ async def peaking_players(message, user, accepted_players, voice_channel_1, voic
         await message.add_reaction(reaction)
 
     current_kapitan = kapitan1_nickname
-    while last_players:
+    while unpeaked_players:
         def check(reaction, user):
             return (
                 user.name == current_kapitan and
@@ -116,46 +107,24 @@ async def peaking_players(message, user, accepted_players, voice_channel_1, voic
         reaction, user = await bot.wait_for('reaction_add', check=check)
         emoji_index = emoji.index(str(reaction.emoji))
 
-        if emoji_index < len(last_players):
-            selected_player = last_players.pop(emoji_index)
+        if emoji_index not in unpeaked_players:
+            await message.channel.send(f"Error: Invalid index {emoji_index}. Try again.")
+            continue
 
-            await message.clear_reaction(reaction.emoji)
+        selected_player = unpeaked_players.pop(emoji_index)
 
-            
+        if current_kapitan == kapitan1_nickname:
+            list_com1.append(selected_player)
+            current_kapitan = kapitan2_nickname
+        else:
+            list_com2.append(selected_player)
+            current_kapitan = kapitan1_nickname
 
-            if current_kapitan == kapitan1_nickname:
-                list_com1.append(selected_player)
-                current_kapitan = kapitan2_nickname
-            else:
-                list_com2.append(selected_player)
-                current_kapitan = kapitan1_nickname
-
-            embed.clear_fields()
-            embed.add_field(name=f'Capitan 1 - @{kapitan1_nickname}', value=f'Players team 1: {", ".join(list_com1)}')
-            embed.add_field(name=f'Capitan 2 - @{kapitan2_nickname}', value=f'Players team 2: {", ".join(list_com2)}')
-            embed.add_field(name='Unpeaked players:', value='\n'.join(last_players) if last_players else 'All players have been picked', inline=False)
-
-            
-
-            await message.edit(embed=embed)
-
-    for el1 in list_com1:    
-        member = discord.utils.get(message.guild.members, name=el1)
-        await member.move_to(voice_channel_1)
-    await message.guild.get_member_named(kapitan1_nickname).move_to(voice_channel_1)
-
-    for el2 in list_com2:    
-        member = discord.utils.get(message.guild.members, name=el2)
-        await member.move_to(voice_channel_2)
-    await message.guild.get_member_named(kapitan2_nickname).move_to(voice_channel_2)
-
-    embed = discord.Embed(title="Game is ready!", description="Here are your teams:", color=discord.Color.purple())
-    embed.set_thumbnail(url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvbwstNLPp77vL3VG5G3H6EVUt705BVF-sEQ&usqp=CAU')
-    embed.set_author(name="discord battle")
-    embed.add_field(name=f'Capitan 1 - @{kapitan1_nickname}', value=f'Players team 1: {", ".join(list_com1)}')
-    embed.add_field(name=f'Capitan 2 - @{kapitan2_nickname}', value=f'Players team 2: {", ".join(list_com2)}')
-
-    await message.channel.send(embed=embed)
-
-
-bot.run('MTEWYB5Fz6-18QpG5h0')
+        await message.clear_reaction(reaction.emoji)
+        embed.clear_fields()
+        embed.add_field(name=f'Capitan 1 - @{kapitan1_nickname}', value=f'Players team 1: {", ".join(list_com1)}')
+        embed.add_field(name=f'Capitan 2 - @{kapitan2_nickname}', value=f'Players team 2: {", ".join(list_com2)}')
+        embed.add_field(name='Unpeaked players:', value='\n'.join(unpeaked_players.values()) if unpeaked_players else 'All players have been picked', inline=False)
+        await message.edit(embed=embed)
+        
+bot.run('MTExMjQtYHbLWYB5Fz6-18QpG5h0')
